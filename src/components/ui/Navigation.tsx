26d1667e84d8d8a1115f13e306ee56ebf702264b
home <catalinpce@gmail.com>
2023-07-24 13:04:57 +0300

    Added auth

--- src/components/ui/Navigation.tsx
+++ src/components/ui/Navigation.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useLocation } from "react-router-dom";
+import firebase from "../../lib/firebase";
 
 const links = [
   { title: "Overview", link: "/" },
@@ -13,20 +14,30 @@ export default function Navigation() {
   const location = useLocation();
 
   return (
-    <div className="flex flex-col">
-      {links.map((link) => {
-        return (
-          <a
-            className={
-              "px-4 py-3 border-b border-neutral-800 text-neutral-400 hover:text-white " +
-              (location.pathname == link.link ? "text-white" : "")
-            }
-            href={link.link}
-          >
-            {link.title}
-          </a>
-        );
-      })}
+    <div className="flex flex-col justify-between w-full h-full">
+      <div className="flex flex-col w-full">
+        {links.map((link) => {
+          return (
+            <a
+              className={
+                "px-4 py-3 border-b border-neutral-800 text-neutral-400 hover:text-white " +
+                (location.pathname == link.link ? "text-white" : "")
+              }
+              href={link.link}
+            >
+              {link.title}
+            </a>
+          );
+        })}
+      </div>
+      <label
+        className={
+          "px-4 py-3 border-b border-t border-neutral-800 text-neutral-400 hover:text-white"
+        }
+        onClick={() => firebase.auth().signOut()}
+      >
+        Sign out
+      </label>
     </div>
   );
 }
